adb3f2bd07e4d73604964aa0e9f1bdf6635161f6
Piyush Kumar Verma <piyush25112006@gmail.com>
2025-12-08 18:19:17 +0530

    a new python file

--- first.py
+++ first.py
@@ -1,3 +1,17 @@
-import pandas as pd
-df=pd.read_csv('C:\Users\user\Downloads\ aboutus.csv')
-df
+n=int(input("enter a number:"))
+i=2
+if n<0:
+    ptint("negative number")
+else:
+    if n<2:
+        print("the no. is not a prime no.")
+    elif n>2:
+        while n>i:
+            if n%i==0:
+                print("the no. is not a prime no.")
+                break
+            i=i+1
+        else:
+            print("the no. is a prime no.")
+    else:
+        print("the no. is a prime no.")
